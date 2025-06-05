bd0c008986e8f1952400eb2c93322b55406fce5a
LUIS FERNANDO MORALES PEREZ <Fernandomoe09@outlook.com>
2025-06-04 21:04:34 -0600

    comentario

---
+++ src/main/java/sysSolutions/Main.java
@@ -0,0 +1,20 @@
+package sysSolutions;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int nota1 =6;
+        int nota2= 9;
+        int nota3= 8;
+        int cal=3;
+       double promedio = (nota1 + nota2 + nota3) /cal;
+
+
+        System.out.print(promedio);
+
+
+
+    }
+}
